bcd17c86595f071a14309a1e8874b50aa3615351
Danex2 <khadane.miller@gmail.com>
2019-05-21 21:48:29 -0400

    added another project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TwitchyIMG from "../images/twitchy.png";
+import WCIMG from '../images/wg.png'
 
 function Projects() {
   return (
@@ -35,18 +36,34 @@ function Projects() {
             Made with: React, Materialize, SASS, Twitch API, Webpack
           </p>
         </div>
-        {/*<div className="project-item">
-          <img src={TwitchyIMG} />
-          <h3 className="project-title">Some project</h3>
-          <p className="project-desc">Some description</p>
-          <p>Made with: React, Bootstrap, Express</p>
-        </div>
         <div className="project-item">
-          <img src={TwitchyIMG} />
-          <h3 className="project-title">Some project</h3>
-          <p className="project-desc">Some description</p>
-          <p>Made with: React, Materialize, Twitch API</p>
-        </div>*/}
+          <img src={WCIMG} alt="Warcraft guilds react project" />
+          <h3 className="project-title">Twitchy</h3>
+          <p className="project-desc">
+            This is a website for people looking for a guild in the game World of Warcraft. I always found that the forums weren't that great for looking for guilds so
+            I made this to make it easier to quickly find a guild to join.
+          </p>
+          <p className="project-links" style={{ fontWeight: "bold" }}>
+            <a
+              href="https://github.com/Danex2/warcraftguilds"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://warcraftguilds.herokuapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Live
+            </a>
+          </p>
+          <p style={{ fontWeight: "bold" }}>
+            Made with: React, Bootstrap, Express
+          </p>
+        </div>
       </div>
     </div>
   );
