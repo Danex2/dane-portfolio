f2e2be1b83bfab331fdc54047709e7c304be5c09
Danex2 <khadane.miller@gmail.com>
2019-05-27 20:41:59 -0400

    fixed twitter link

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
             </div>
             <div class="icon-container">
                 <a href="https://github.com/Danex2" class="fab fa-github fa-3x home-icon" target="_blank" rel="noopener noreferrer"></a>
-                <a href="https:/twitter.com/hybridearth" class="fab fa-twitter-square fa-3x home-icon" target="_blank" rel="noopener noreferrer"></a>
+                <a href="https://twitter.com/hybridearth" class="fab fa-twitter-square fa-3x home-icon" target="_blank" rel="noopener noreferrer"></a>
                 <a href="https://codepen.io/Danex2" class="fab fa-codepen fa-3x home-icon" target="_blank" rel="noopener noreferrer"></a>
                 <a href="mailto:khadane.miller@gmail.com?Subject=Hello!" class="fas fa-envelope fa-3x home-icon" target="_blank" rel="noopener noreferrer"></a>
             </div>
